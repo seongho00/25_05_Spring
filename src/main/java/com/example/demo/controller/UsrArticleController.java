4ae0ae85dd7077cdb1aa889acaeb660fa89cbacd
seongho00 <tjdwns000927@gmail.com>
2025-05-07 13:42:48 +0900

    testData, 삭제, 수정 구현

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -21,6 +21,16 @@ public class UsrArticleController {
 
 	}
 
+	@RequestMapping("/usr/article/CreateTestData")
+	@ResponseBody
+	public String CreateTestData() {
+		for (int i = 1; i <= 10; i++) {
+			articles.add(new Article(++id, "제목" + i, "내용" + i));
+		}
+
+		return "테스트 데이터 생성됨";
+	}
+
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public String doAdd(String title, String body) {
@@ -37,8 +47,32 @@ public class UsrArticleController {
 		return articles;
 	}
 
-}
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
 
-class CreateTestData {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				articles.remove(article);
+				return id + "번 게시글이 삭제되었습니다.";
+			}
+		}
+
+		return id + "번 게시글은 없습니다.";
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public String doModify(int id, String title, String body) {
+
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				article.setTitle(title);
+				article.setBody(body);
+				return id + "번 게시글이 수정되었습니다." + article;
+			}
+		}
+		return id + "번 게시글은 없습니다.";
+	}
 
 }
